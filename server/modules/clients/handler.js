12fdbe4a0ae4da61d63b5440141daa2dcdfa2733
Josh Merrell <joshmerrell.us@gmail.com>
2023-05-19 20:57:34 -0700

    05192023_cleanup-prepping for supabase init

--- server/modules/clients/handler.js
+++ server/modules/clients/handler.js
@@ -35,7 +35,16 @@ async function createClient(req, res) {
   return res.json(returner);
 }
 
+async function deleteClient(req, res) {
+  const db = req.client.db;
+  const processor = require('./index')({ db });
+  const { clientID } = req.params;
+  const returner = await processor.delete({ clientID });
+  return res.json(returner);
+}
+
 module.exports = {
   getClients,
   createClient,
+  deleteClient,
 };
