ec111b4047c55ae3c08f4bda72889ef64dd89fd3
Josh Merrell <27826752+josh-merrell@users.noreply.github.com>
2023-05-24 20:53:41 -0700

    05242023_recipe Endpoints done, validated (#13)

--- server/modules/recipes/router.js
+++ server/modules/recipes/router.js
@@ -1,30 +1,29 @@
 const express = require('express');
 
-// const { routeValidator } = require('../../middleware/validating');
-// const { errorCatcher } = require('../../middleware/errorHandling');
-// const handler = require('./handler');
-// const {
-//   getRecipesSchema_query,
-//   getRecipeSchema_params,
-//   newRecipeSchema_body,
-//   RecipeUpdateSchema_body,
-//   RecipeUpdateSchema_params,
-//   RecipeDeleteSchema_params,
-// } = require('../../schemas/recipe-types');
+const { routeValidator } = require('../../middleware/validating');
+const { errorCatcher } = require('../../middleware/errorHandling');
+const handler = require('./handler');
+const {
+  getRecipesSchema_query,
+  getRecipeSchema_params,
+  newRecipeSchema_body,
+  RecipeUpdateSchema_body,
+  RecipeUpdateSchema_params,
+  RecipeDeleteSchema_params,
+} = require('../../schemas/recipe-types');
 const recipeCategoriesRouter = require('./categories/router');
 // const recipeComponentsRouter = require('./components/router');
 
 const router = express.Router();
-// const h = handler;
+const h = handler;
 
 router.use('/categories', recipeCategoriesRouter);
 // router.use('/components', recipeComponentsRouter);
 
-// router.get('/:recipeID', routeValidator(getRecipeSchema_params, 'params'), errorCatcher(h.getRecipeByID));
-// router.get('/', routeValidator(getRecipesSchema_query, 'query'), errorCatcher(h.getRecipes));
-// router.post('/', routeValidator(newRecipeSchema_body, 'body'), errorCatcher(h.createRecipe));
-
-// router.patch('/:recipeID', routeValidator(RecipeUpdateSchema_body, 'body'), routeValidator(RecipeUpdateSchema_params, 'params'), errorCatcher(h.updateRecipe));
-// router.delete('/:recipeID', routeValidator(RecipeDeleteSchema_params, 'params'), errorCatcher(h.deleteRecipe));
+router.get('/:recipeID', routeValidator(getRecipeSchema_params, 'params'), errorCatcher(h.getRecipeByID));
+router.patch('/:recipeID', routeValidator(RecipeUpdateSchema_body, 'body'), routeValidator(RecipeUpdateSchema_params, 'params'), errorCatcher(h.updateRecipe));
+router.delete('/:recipeID', routeValidator(RecipeDeleteSchema_params, 'params'), errorCatcher(h.deleteRecipe));
+router.get('/', routeValidator(getRecipesSchema_query, 'query'), errorCatcher(h.getRecipes));
+router.post('/', routeValidator(newRecipeSchema_body, 'body'), errorCatcher(h.createRecipe));
 
 module.exports = router;
