4d061a047ff9afc641a0e8b6a511a3046dd3c908
Josh Merrell <27826752+josh-merrell@users.noreply.github.com>
2023-05-24 19:37:36 -0700

    05242023_recipeCategory_endpoints (#12)

---
+++ server/modules/recipes/categories/router.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const { routeValidator } = require('../../../middleware/validating');
+const { errorCatcher } = require('../../../middleware/errorHandling');
+const handler = require('./handler');
+const {
+  getRecipeCategorySchema_params,
+  newRecipeCategorySchema_body,
+  RecipeCategoryUpdateSchema_body,
+  RecipeCategoryUpdateSchema_params,
+  RecipeCategoryDeleteSchema_params,
+} = require('../../../schemas/recipe-types');
+
+const router = express.Router();
+const h = handler;
+
+router.get('/:recipeCategoryID', routeValidator(getRecipeCategorySchema_params, 'params'), errorCatcher(h.getRecipeCategoryByID));
+router.get('/', errorCatcher(h.getRecipeCategories));
+router.post('/', routeValidator(newRecipeCategorySchema_body, 'body'), errorCatcher(h.createRecipeCategory));
+router.patch(
+  '/:recipeCategoryID',
+  routeValidator(RecipeCategoryUpdateSchema_body, 'body'),
+  routeValidator(RecipeCategoryUpdateSchema_params, 'params'),
+  errorCatcher(h.updateRecipeCategory),
+);
+router.delete('/:recipeCategoryID', routeValidator(RecipeCategoryDeleteSchema_params, 'params'), errorCatcher(h.deleteRecipeCategory));
+
+module.exports = router;
