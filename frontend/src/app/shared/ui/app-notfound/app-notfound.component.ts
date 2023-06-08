845e6c3807b3f6d2161e155f1ddb968216d85d31
Josh Merrell <27826752+josh-merrell@users.noreply.github.com>
2023-06-08 12:42:29 -0700

    06082023_header scaffolding done, rdy for ts (#27)

---
+++ frontend/src/app/shared/ui/app-notfound/app-notfound.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-notfound',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app-notfound.component.html',
+})
+export class NotfoundComponent {
+
+}
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,14 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/routes';
 
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    // provideStore({ router: routerReducer, auth: authReducer }),
+    // provideRouterStore(),
+    // provideEffects([RouterEffects, AuthEffects])
+  ],
+})
+  .catch((err) => console.error(err));
