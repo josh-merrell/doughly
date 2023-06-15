e25b7ccf7c65b7bb92b66b15578dd8b72c221e91
Josh Merrell <27826752+josh-merrell@users.noreply.github.com>
2023-06-14 23:06:35 -0700

    06142023 _ implemented client-side auth using Supabase client. Signin/Logout. Also wrote HTTP Interceptor to add supa token to API requests, but need to receive and process "api_token" on server side, then test (#34)

---
+++ frontend/src/app/guards/profile.guard.ts
@@ -0,0 +1,36 @@
+import { inject } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateChildFn,
+  CanActivateFn,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { first, map } from 'rxjs';
+import { AuthService } from '../shared/utils/authenticationService';
+
+export const canActivate: CanActivateFn = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  return authService.$profile.pipe(
+    // We only want to get the first emitted value from the $profile
+    first(),
+    map((profile) => {
+      // Allow access if the user's profile is set
+      if (profile) return true;
+      else {
+        // Redirect to the /login route, while capturing the current url so we can redirect after login
+        router.navigate(['/login'], {
+          queryParams: { redirect_url: state.url },
+        });
+        return false;
+      }
+    })
+  )
+}
+
+export const ProfileGuard: CanActivateChildFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => canActivate(route, state);
---
+++ frontend/src/app/login/login-page.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthService } from '../shared/utils/authenticationService';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'dl-login-page',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login-page.component.html',
+})
+export class LoginPageComponent {
+  constructor(private router: Router, private authService: AuthService) {}
+
+  login_form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  error?: string;
+
+  onSubmit() {
+    if (this.login_form.valid) {
+      delete this.error;
+
+      const { email, password } = this.login_form.value;
+      this.authService
+        .signIn(email!, password!)
+        .then(() => {
+          this.router.navigate(['/']);
+        })
+        .catch((error) => {
+          this.error = error.message;
+        });
+    }
+  }
+}
