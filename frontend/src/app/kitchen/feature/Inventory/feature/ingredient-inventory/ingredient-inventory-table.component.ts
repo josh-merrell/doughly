b2a79311c42979836278767c3515ebb5232ab54b
Josh Merrell <joshmerrell.us@gmail.com>
2023-07-03 13:15:09 -0700

    07032023_Ingredient Inventory GET done

--- frontend/src/app/kitchen/feature/Inventory/feature/ingredient-inventory/ingredient-inventory-table.component.ts
+++ frontend/src/app/kitchen/feature/Inventory/feature/ingredient-inventory/ingredient-inventory-table.component.ts
@@ -5,6 +5,13 @@ import { environment } from 'src/environments/environment';
 import { AuthService } from 'src/app/shared/utils/authenticationService';
 import { TableFullComponent } from 'src/app/shared/ui/dl-table-full/dl-table-full.component';
 import { IngredientsComponent } from 'src/app/ingredients/feature/ingredients/ingredients.component';
+import { IngredientStock, IngredientStockRow } from './state/ingredient-stock-state';
+import { Observable } from 'rxjs';
+import { Ingredient } from 'src/app/ingredients/state/ingredient-state';
+import { Store, select } from '@ngrx/store';
+import { selectIngredients } from 'src/app/ingredients/state/ingredient-selectors';
+import { selectIngredientStocks } from './state/ingredient-stock-selectors';
+import { IngredientStockService } from './data/ingredient-stock.service';
 
 @Component({
   selector: 'dl-ingredient-inventory-table',
@@ -13,13 +20,23 @@ import { IngredientsComponent } from 'src/app/ingredients/feature/ingredients/in
   templateUrl: './ingredient-inventory-table.component.html',
 })
 export class IngredientInventoryTableComponent {
-  constructor(private http: HttpClient, private auth: AuthService) {}
+  constructor(private http: HttpClient, private auth: AuthService, private store: Store, private ingredientStockService: IngredientStockService) {}
 
   title = 'Ingredient Inventory';
   heading_phrase = 'Multiple entries may exist for each Ingredient.';
-  button_title = 'Add Ingredient';
+  button_title = 'Add Inventory';
+  columns = [
+    { name: 'Name', prop: 'name' },
+    { name: 'Brand', prop: 'brand' },
+    { name: 'Quantity', prop: 'quantity' },
+    { name: 'Expiration', prop: 'expiration' },
+  ]
 
-  ngOnInit() {}
+  rows$: Observable<IngredientStockRow[]> = this.ingredientStockService.rows$;
+
+
+  ngOnInit() {
+  }
 
   sendRequest = () => {
     console.log('Sending request');
--- frontend/src/app/kitchen/feature/Inventory/feature/ingredient-inventory/state/ingredient-stock-selectors.ts
+++ frontend/src/app/kitchen/feature/Inventory/feature/ingredient-inventory/state/ingredient-stock-selectors.ts
@@ -1,9 +1,9 @@
 import { createSelector } from '@ngrx/store';
-import { IngredientStockState } from './ingredient-stock-state';
+import { IngredientStockState, IngredientStock } from './ingredient-stock-state';
 
-export const selectIngredientStocks = (state: IngredientStockState) => state.ingredientStocks;
+export const selectIngredientStocks = (state: any) => state.ingredientStock.ingredientStocks;
 
 export const selectIngredientStockByID = (ingredientStockID: number) =>
   createSelector(selectIngredientStocks, (ingredientStocks) =>
-    ingredientStocks.find((ingredientStock) => ingredientStock.ingredientStockID === ingredientStockID)
+    ingredientStocks.find((ingredientStock: IngredientStock) => ingredientStock.ingredientStockID === ingredientStockID)
   );
