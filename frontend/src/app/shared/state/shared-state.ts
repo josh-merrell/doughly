9b167f6cb0fdde635f5de32a593ba7e940728e88
Josh Merrell <27826752+josh-merrell@users.noreply.github.com>
2023-07-08 15:19:42 -0700

    Table sorting (#42)
    * 07072023_added 'deleted' to all tables, finished DeleteIngredientStockModal
    
    * 07082023_addedNestedTableSorting

--- frontend/src/app/shared/state/shared-state.ts
+++ frontend/src/app/shared/state/shared-state.ts
@@ -1,3 +1,34 @@
 export interface SharedState {
   currentUrl: string;
 }
+export interface Sort {
+  prop: string;
+  sortOrderIndex: number;
+  direction: string;
+}
+
+export enum SortEnum {
+  alphabetical = 'alphabetical',
+  numberical = 'numberical',
+}
+
+export enum FilterEnum {
+  search = 'search',
+  numRange = 'numRange',
+  dateRange = 'dateRange',
+  none = 'none',
+}
+
+export enum SortRotateStateEnum {
+  default = 'default',
+  down = 'down',
+  up = 'up',
+}
+export interface TableFullColumn {
+  name: string;
+  prop: string;
+  sort?: SortEnum;
+  sortRotateState?: SortRotateStateEnum;
+  sortOrderState?: number | null;
+  filter: FilterEnum;
+}
