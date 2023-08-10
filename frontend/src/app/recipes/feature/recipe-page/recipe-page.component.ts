c10a7ba595ff0f423fb385fc4e45a4aa3d575a01
Josh Merrell <joshmerrell.us@gmail.com>
2023-08-09 20:54:59 -0700

    08092023: - Added table headers with columns for Recipe table - Adjusted dynamic width strategy for Tables

--- frontend/src/app/recipes/feature/recipe-page/recipe-page.component.ts
+++ frontend/src/app/recipes/feature/recipe-page/recipe-page.component.ts
@@ -1,8 +1,21 @@
-import { Component, ElementRef, HostListener, Renderer2, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RecipesInfoComponent } from './ui/recipes-info/recipes-info.component';
 import { RecipeCategoryService } from '../../data/recipe-category.service';
-import { Observable, Subscription } from 'rxjs';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 import {
   RecipeCategory,
   RecipeCategoryError,
@@ -21,10 +34,16 @@ import { UpdateRequestConfirmationModalComponent } from 'src/app/shared/ui/updat
 import { DeleteRequestErrorModalComponent } from 'src/app/shared/ui/delete-request-error/delete-request-error-modal.component';
 import { DeleteRequestConfirmationModalComponent } from 'src/app/shared/ui/delete-request-confirmation/delete-request-confirmation-modal.component';
 import {
-  selectView,
-} from '../../state/recipe-page-selectors';
+  FilterTypeEnum,
+  Sort,
+  SortEnum,
+  SortRotateStateEnum,
+  TableFullColumn,
+} from 'src/app/shared/state/shared-state';
+import { selectView } from '../../state/recipe-page-selectors';
 import { RecipePageActions } from '../../state/recipe-page-actions';
 import { FormsModule } from '@angular/forms';
+import { SortingService } from 'src/app/shared/utils/sortingService';
 
 function isRecipeCategoryError(obj: any): obj is RecipeCategoryError {
   return obj && obj.errorType !== undefined && obj.message !== undefined;
@@ -34,7 +53,29 @@ function isRecipeCategoryError(obj: any): obj is RecipeCategoryError {
   standalone: true,
   imports: [CommonModule, RecipesInfoComponent, FormsModule],
   templateUrl: './recipe-page.component.html',
-  styleUrls: ['./recipe-page.component.scss'],
+  animations: [
+    trigger('rotateState', [
+      state(
+        'default',
+        style({
+          transform: 'rotate(0)',
+        })
+      ),
+      state(
+        'down',
+        style({
+          transform: 'rotate(90deg)',
+        })
+      ),
+      state(
+        'up',
+        style({
+          transform: 'rotate(270deg)',
+        })
+      ),
+      transition('* => *', animate('0.3s ease')),
+    ]),
+  ],
 })
 export class RecipePageComponent {
   view$: Observable<string> = this.store.select(selectView);
@@ -47,24 +88,63 @@ export class RecipePageComponent {
   private recipeCategoriesSubscription?: Subscription;
   viewSubscription!: Subscription;
   searchBarSubscription!: Subscription;
+  columns: TableFullColumn[] = [];
   searchFilter: string = '';
+  sorts: Sort[] = [];
+  SortEnum = SortEnum;
+  SortRotateStateEnum = SortRotateStateEnum;
+  public clearAllSortsEnabled = false;
+  public rows$ = new BehaviorSubject<any[]>([]);
+  public filteredRows$ = new BehaviorSubject<any[]>([]);
+  public displayedRows$ = new BehaviorSubject<any[]>([]);
+  modalActiveForRowID: number | null = null;
 
   constructor(
     private renderer: Renderer2,
     private recipeCategoryService: RecipeCategoryService,
     private store: Store,
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    private sortingService: SortingService
   ) {
     this.recipeCategoriesSubscription = this.recipeCategories$.subscribe(
       (categories) => {
         this.recipeCategories = categories;
       }
     );
+
+    this.columns = [
+      {
+        name: 'ID',
+        prop: 'recipeID',
+        cssClass: 'w-1/5',
+        sort: SortEnum.numerical,
+        sortRotateState: SortRotateStateEnum.default,
+        sortOrderState: null,
+        filterType: FilterTypeEnum.search,
+      },
+      {
+        name: 'Recipe',
+        prop: 'recipeName',
+        cssClass: 'w-2/5',
+        sort: SortEnum.alphabetical,
+        sortRotateState: SortRotateStateEnum.default,
+        sortOrderState: null,
+        filterType: FilterTypeEnum.search,
+      },
+      {
+        name: 'Category',
+        prop: 'recipeCategoryName',
+        cssClass: 'w-2/5',
+        sort: SortEnum.alphabetical,
+        sortRotateState: SortRotateStateEnum.default,
+        sortOrderState: null,
+        filterType: FilterTypeEnum.search,
+      },
+    ];
   }
 
   totalRecipes = 5; //TODO: get this from the store
   readyToMake = 3; //TODO: get this from the store
-  modalActiveForRowID: number | null = null;
   categoryMenuOpen = { index: -1, open: false };
   @ViewChild('categoryMenu') categoryMenu!: ElementRef;
   globalClickListener: () => void = () => {};
@@ -253,6 +333,119 @@ export class RecipePageComponent {
         });
       }
     });
+  }
+
+  getClass(column: any, i: number) {
+    return {
+      'rounded-tl-dl-3': i === 0,
+      [column.cssClass]: column.cssClass,
+    };
+  }
+
+  getRowClass(column: any, i: number) {
+    return {
+      'bg-dl-grey-9': this.modalActiveForRowID === i,
+      [column.cssClass]: column.cssClass,
+    };
+  }
+
+  onSortIconClick(rowIndex: number): void {
+    //first update direction of sort
+    this.updateSort(rowIndex);
+    //then call appropriate method to add, update, or clear sorting
+    switch (this.columns[rowIndex].sortRotateState) {
+      case SortRotateStateEnum.up:
+        this.addSort(rowIndex);
+        break;
+      case SortRotateStateEnum.down:
+        this.updateSortedRows();
+        break;
+      case SortRotateStateEnum.default:
+        this.clearSort(Number(this.columns[rowIndex].sortOrderState));
+        break;
+    }
+  }
+
+  updateSort(colIndex: number) {
+    switch (this.columns[colIndex].sortRotateState) {
+      case SortRotateStateEnum.default:
+        this.columns[colIndex].sortRotateState = SortRotateStateEnum.up;
+        //find the corresponding sort and update its direction
+        for (let i = 0; i < this.sorts.length; i++) {
+          if (this.sorts[i].prop === this.columns[colIndex].prop) {
+            this.sorts[i].direction = 'asc';
+          }
+        }
+        break;
+      case SortRotateStateEnum.down:
+        this.columns[colIndex].sortRotateState = SortRotateStateEnum.default;
+        break;
+      case SortRotateStateEnum.up:
+        this.columns[colIndex].sortRotateState = SortRotateStateEnum.down;
+        for (let i = 0; i < this.sorts.length; i++) {
+          if (this.sorts[i].prop === this.columns[colIndex].prop) {
+            this.sorts[i].direction = 'desc';
+          }
+        }
+        break;
+    }
+  }
+
+  addSort(rowIndex: number): void {
+    const newSort: Sort = {
+      prop: this.columns[rowIndex].prop,
+      sortOrderIndex: this.sorts.length,
+      direction: 'asc',
+    };
+    this.sorts.push(newSort);
+    this.columns[rowIndex].sortOrderState = newSort.sortOrderIndex;
+    //Apply the sorts to rows, then emit the sorted rows
+    const sortedRows = this.sortingService.applySorts(
+      this.filteredRows$.getValue(),
+      this.sorts
+    );
+    this.displayedRows$.next(sortedRows);
+    this.clearAllSortsEnabled = this.sorts.length > 0;
+  }
+
+  updateSortedRows(): void {
+    const sortedRows = this.sortingService.applySorts(
+      this.filteredRows$.getValue(),
+      this.sorts
+    );
+    this.displayedRows$.next(sortedRows);
+  }
+
+  clearSort(index: number): void {
+    for (let i = index + 1; i < this.sorts.length; i++) {
+      this.sorts[i].sortOrderIndex--;
+    }
+    this.sorts.splice(index, 1);
+    this.columns.forEach((column) => {
+      if (column.sortOrderState! > index) {
+        column.sortOrderState!--;
+      }
+    });
+    //Apply the sorts to rows, then emit the sorted rows
+    const sortedRows = this.sortingService.applySorts(
+      this.filteredRows$.getValue(),
+      this.sorts
+    );
+    this.clearAllSortsEnabled = this.sorts.length > 0;
+    this.displayedRows$.next(sortedRows);
+  }
+
+  clearAllSorts(): void {
+    this.sorts = [];
+    this.columns.forEach((column) => {
+      if (column.sort) {
+        column.sortOrderState = null;
+        column.sortRotateState = SortRotateStateEnum.default;
+      }
+    });
+    //Return sortedRows to default, matching rows
+    this.displayedRows$.next(this.filteredRows$.getValue());
+    this.clearAllSortsEnabled = false;
   }
 
   ngOnDestroy() {
