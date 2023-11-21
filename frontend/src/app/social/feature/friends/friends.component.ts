ccce662c73e28024ef9dbeb947668d8968c5febc
Josh Merrell <joshmerrell.us@gmail.com>
2023-11-21 12:29:47 -0800

    11212023: - Header Menu Now closes when navigating - Social Components Skeleton

---
+++ frontend/src/app/social/feature/friends/friends.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'dl-friends',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './friends.component.html',
+})
+export class FriendsComponent {
+
+}
---
+++ frontend/src/app/social/feature/friends/ui/friend-modal/friend-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'dl-friend-modal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './friend-modal.component.html',
+})
+export class FriendModalComponent {
+
+}
