e8d6d9770ee0dc1d8bab558afa91457f69c71016
Josh Merrell <joshmerrell.us@gmail.com>
2023-12-24 07:23:31 -0800

    12242023: - social url routing - fixed errors when draft > shopping

--- frontend/src/app/social/social-page.component.ts
+++ frontend/src/app/social/social-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, WritableSignal, signal } from '@angular/core';
+import { Component, WritableSignal, effect, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Store } from '@ngrx/store';
 import { FriendshipActions } from './state/friendship-actions';
@@ -7,6 +7,8 @@ import { FriendsComponent } from './feature/friends/friends.component';
 import { FollowersComponent } from './feature/followers/followers.component';
 import { TimelineComponent } from './feature/timeline/timeline.component';
 import { RecipeActions } from '../recipes/state/recipe/recipe-actions';
+import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'dl-social-page',
@@ -16,16 +18,25 @@ import { RecipeActions } from '../recipes/state/recipe/recipe-actions';
     FriendsComponent,
     FollowersComponent,
     TimelineComponent,
+    RouterModule,
   ],
   templateUrl: './social-page.component.html',
 })
 export class SocialPageComponent {
   public view: WritableSignal<string> = signal('friends');
 
-  constructor(private store: Store) {}
-
-  ngOnInit() {
+  constructor(private router: Router, private store: Store) {
+    effect(() => {
+      const view = this.view();
+      if (view === 'friends') {
+        this.router.navigate(['/social/friends']);
+      } else if (view === 'followers') {
+        this.router.navigate(['/social/followers']);
+      }
+    });
   }
+
+  ngOnInit() {}
 
   setView(view: string) {
     this.view.set(view);
