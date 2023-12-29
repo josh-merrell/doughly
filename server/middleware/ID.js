fa4f9e5c8dcfc3e00332677695ca575c0bdb68c9
Josh Merrell <27826752+josh-merrell@users.noreply.github.com>
2023-12-29 15:11:58 -0800

    fix backend ID.js 'process.env.NODE_ENV' bug (#211)

--- server/middleware/ID.js
+++ server/middleware/ID.js
@@ -57,7 +57,7 @@ async function generateID(req, res, next) {
   // get current UTC time date
   const now = new Date(Date.now() - new Date().getTimezoneOffset() * 60000);
   let year = now.getUTCFullYear().toString().slice(-2);
-  if (env.NODE_ENV === 'development') {year -= 20};
+  if (process.env.NODE_ENV === 'development') {year -= 20};
   const month = String(now.getUTCMonth() + 1).padStart(2, '0');
   const day = String(now.getUTCDate()).padStart(2, '0');
   let sequence = await getNextSequence(type);
