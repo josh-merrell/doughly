3a307546b75b921b7a0dd85464b79e0e48b62127
Josh Merrell <27826752+josh-merrell@users.noreply.github.com>
2023-12-29 23:00:35 -0800

    Josh (#213)
    * - Google Auth working
    
    * style tweak

--- frontend/src/app/account/feature/login/login-page.component.ts
+++ frontend/src/app/account/feature/login/login-page.component.ts
@@ -28,6 +28,7 @@ import { ToolStockActions } from '../../.././kitchen/feature/Inventory/feature/t
 import { RecipeToolActions } from '../../.././recipes/state/recipe-tool/recipe-tool-actions';
 import { RecipeStepActions } from '../../.././recipes/state/recipe-step/recipe-step-actions';
 import { ShoppingListActions } from '../../.././groceries/state/shopping-list-actions';
+import { CredentialResponse, PromptMomentNotification } from 'google-one-tap';
 
 @Component({
   selector: 'dl-login-page',
@@ -43,32 +44,52 @@ import { ShoppingListActions } from '../../.././groceries/state/shopping-list-ac
   templateUrl: './login-page.component.html',
 })
 export class LoginPageComponent {
-  private googleScriptId = 'google-signin-script';
+  error?: string;
   constructor(
     private store: Store,
     private router: Router,
     private authService: AuthService,
     private ngZone: NgZone
-  ) {
-    // Expose the Google Login callback function to the global scope
-    window['handleSignInWithGoogle'] = (response) =>
-      this.handleSignInWithGoogle(response);
-  }
+  ) {}
 
   ngOnInit() {
-    this.loadGoogleSignInScript();
+    // @ts-ignore
+    window.onGoogleLibraryLoad = () => {
+      console.log("Google's One-tap sign in script loaded!");
+
+      // @ts-ignore
+      google.accounts.id.initialize({
+        // Ref: https://developers.google.com/identity/gsi/web/reference/js-reference#IdConfiguration
+        client_id:
+          '911585064385-1ei5d9gdp9h1igf9hb7hqfqp466j6l0v.apps.googleusercontent.com',
+        callback: this.handleSignInWithGoogle.bind(this), //this callback will use supabase method
+        auto_select: false,
+        cancel_on_tap_outside: true,
+      });
+      // @ts-ignore
+      google.accounts!.id.renderButton(document!.getElementById('g_loginBtn'), {
+        theme: 'outline',
+        size: 'large',
+        type: 'icon',
+        shape: 'rectangular',
+        width: "100"
+      });
+      // @ts-ignore
+      google.accounts.id.prompt();
+    };
   }
 
-  ngOnDestroy() {
-    this.removeGoogleSignInScript();
-  }
-
-  handleSignInWithGoogle(response: any) {
+  handleSignInWithGoogle(response: CredentialResponse) {
     // Extract the credential (token) from the response
     const token = response.credential;
-
+    let decodedToken: any | null = null;
+    try {
+      decodedToken = JSON.parse(atob(token.split('.')[1]));
+      console.log('Decoded token:', decodedToken);
+    } catch (error) {
+      console.log('Error decoding token:', error);
+    }
     // Optional: You might want to handle nonce here if you use it
-
     // Use the token to sign in with Supabase
     this.ngZone.run(() => {
       this.authService
@@ -85,43 +106,10 @@ export class LoginPageComponent {
     });
   }
 
-  private loadGoogleSignInScript() {
-    if (document.getElementById(this.googleScriptId)) {
-      // If the script is already present, there's no need to load it again
-      return;
-    }
-
-    const scriptElement = document.createElement('script');
-    scriptElement.src = 'https://accounts.google.com/gsi/client';
-    scriptElement.async = true;
-    scriptElement.defer = true;
-    scriptElement.id = this.googleScriptId;
-    document.body.appendChild(scriptElement);
-
-    scriptElement.onload = () => {
-      // Initialize the Google sign-in functionality here, if needed
-      // For example:
-      // google.accounts.id.initialize({
-      //   client_id: '911585064385-1ei5d9gdp9h1igf9hb7hqfqp466j6l0v.apps.googleusercontent.com',
-      //   callback: (response) => this.handleSignInWithGoogle(response)
-      // });
-    };
-  }
-
-  private removeGoogleSignInScript() {
-    const scriptElement = document.getElementById(this.googleScriptId);
-    if (scriptElement) {
-      document.body.removeChild(scriptElement);
-    }
-  }
-
   login_form = new FormGroup({
-    // email: new FormControl('', [Validators.required, Validators.email]),
     email: new FormControl('', [Validators.required]),
     password: new FormControl('', [Validators.required]),
   });
-
-  error?: string;
 
   onSubmit() {
     if (this.login_form.valid) {
