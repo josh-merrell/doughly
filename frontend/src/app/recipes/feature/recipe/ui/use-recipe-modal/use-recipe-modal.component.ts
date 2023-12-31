47677db4729d8b4508a0c72698793612de912a6f
Josh Merrell <joshmerrell.us@gmail.com>
2023-12-30 23:45:15 -0800

    added generic Error component that takes message and statusCode

--- frontend/src/app/recipes/feature/recipe/ui/use-recipe-modal/use-recipe-modal.component.ts
+++ frontend/src/app/recipes/feature/recipe/ui/use-recipe-modal/use-recipe-modal.component.ts
@@ -24,9 +24,12 @@ import { Store } from '@ngrx/store';
 import { IngredientStockActions } from 'src/app/kitchen/feature/Inventory/feature/ingredient-inventory/state/ingredient-stock-actions';
 import { RecipeActions } from 'src/app/recipes/state/recipe/recipe-actions';
 import { concatMap, filter, switchMap, take, tap } from 'rxjs';
-import { selectUpdating, selectError } from 'src/app/recipes/state/recipe/recipe-selectors';
-
-
+import {
+  selectUpdating,
+  selectError,
+} from 'src/app/recipes/state/recipe/recipe-selectors';
+import { ErrorComponent } from 'src/app/shared/ui/error/error.component';
+import { AddRequestConfirmationModalComponent } from 'src/app/shared/ui/add-request-confirmation/add-request-confirmation-modal.component';
 
 @Component({
   selector: 'dl-use-recipe-modal',
@@ -75,22 +78,42 @@ export class UseRecipeModalComponent {
     const difficulty = this.form.get('difficulty')?.value;
     const note = this.form.get('note')?.value;
 
-    this.store.dispatch(RecipeActions.useRecipe({ recipeID: this.data.recipeID, satisfaction, difficulty, note }));
-    this.store.select(selectUpdating).pipe(
-      filter(updating => !updating),
-      take(1)
-    ).subscribe(() => {
-      this.store.select(selectError).pipe(
-        take(1)
-      ).subscribe((error) => {
-        if (error) {
-          console.error(`Recipe use failed: ${error.message}, CODE: ${error.statusCode}`)
-        } else {
-          console.log(`Recipe successfully used`);
-        }
-        this.isSubmitting = false;
+    this.store.dispatch(
+      RecipeActions.useRecipe({
+        recipeID: this.data.recipeID,
+        satisfaction,
+        difficulty,
+        note,
       })
-    })
+    );
+    this.store
+      .select(selectUpdating)
+      .pipe(
+        filter((updating) => !updating),
+        take(1)
+      )
+      .subscribe(() => {
+        this.store
+          .select(selectError)
+          .pipe(take(1))
+          .subscribe((error) => {
+            if (error) {
+              console.error(
+                `Recipe use failed: ${error.message}, CODE: ${error.statusCode}`
+              );
+              this.dialog.open(ErrorComponent, {
+                maxWidth: '380px',
+                data: {
+                  errorMessage: error.message,
+                  statusCode: error.statusCode,
+                },
+              });
+            } else {
+              this.dialogRef.close('success');
+            }
+            this.isSubmitting = false;
+          });
+      });
   }
 
   onCancel() {
