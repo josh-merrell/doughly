ab32ce3f2f5826b098aa7b0819842d254763d98d
Josh Merrell <27826752+josh-merrell@users.noreply.github.com>
2024-01-17 01:34:11 -0500

    Josh (#229)
    * 01162024:
    - WIP Recipe Add Method Selection
    
    * - component tested. Next need to complete visionAddRecipeModal

---
+++ frontend/src/app/recipes/feature/recipes-page/ui/recipe/vision-add-recipe-modal/vision-add-recipe-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dl-vision-add-recipe-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './vision-add-recipe-modal.component.html',
+})
+export class VisionAddRecipeModalComponent {
+
+}
