d69915082b38394a5fd5a9ae0103fa7aa8b598f2
Josh Merrell <joshmerrell.us@gmail.com>
2024-03-27 17:49:18 -0700

    - test

--- frontend/src/app/footer/feature/app-footer.component.ts
+++ frontend/src/app/footer/feature/app-footer.component.ts
@@ -21,6 +21,15 @@ import { selectCurrentUrl } from '../../shared/state/shared-selectors';
 import { AppState } from '../../shared/state/app-state';
 import { AuthService } from '../../shared/utils/authenticationService';
 import { selectAdding } from '../../recipes/state/recipe/recipe-selectors';
+
+
+import {
+  ActionPerformed,
+  PushNotificationSchema,
+  PushNotifications,
+  Token,
+} from '@capacitor/push-notifications';
+
 @Component({
   selector: 'app-footer',
   standalone: true,
@@ -57,7 +66,7 @@ export class AppFooterComponent {
   ngOnInit() {
     // watch location for changes
     this.location.onUrlChange((url) => {
-      this.currentLocation.set(url)
+      this.currentLocation.set(url);
     });
     this.store.select(selectAdding).subscribe((adding) => {
       this.addingRecipe.set(adding);
@@ -83,6 +92,47 @@ export class AppFooterComponent {
         })
       )
       .subscribe();
+
+    //** INIT NOTIFICATION METHODS
+    // Request permission to use push notifications
+    // iOS will prompt user and return if they granted permission or not
+    // Android will just grant without prompting
+    PushNotifications.requestPermissions().then((result) => {
+      if (result.receive === 'granted') {
+        // Register with Apple / Google to receive push via APNS/FCM
+        PushNotifications.register();
+      } else {
+        // Show some error
+      }
+    });
+    // On success, we should be able to receive notifications
+    PushNotifications.addListener('registration', (token: Token) => {
+      alert('Push registration success, token: ' + token.value);
+      // Send the token to the server
+      this.authService
+        .updateProfile({
+          pushToken: token.value,
+        })
+        .subscribe();
+    });
+    // Some issue with our setup and push will not work
+    PushNotifications.addListener('registrationError', (error: any) => {
+      alert('Error on registration: ' + JSON.stringify(error));
+    });
+    // Show us the notification payload if the app is open on our device
+    PushNotifications.addListener(
+      'pushNotificationReceived',
+      (notification: PushNotificationSchema) => {
+        alert('Push received: ' + JSON.stringify(notification));
+      }
+    );
+    // Method called when tapping on a notification
+    PushNotifications.addListener(
+      'pushNotificationActionPerformed',
+      (notification: ActionPerformed) => {
+        alert('Push action performed: ' + JSON.stringify(notification));
+      }
+    );
   }
 
   ngAfterViewInit() {
