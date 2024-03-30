027a430b5e63e028b6606d2e246b859414373d93
Josh Merrell <joshmerrell.us@gmail.com>
2024-03-30 10:47:56 -0700

    03302024: - abstracted function for getting tokens, filtering based on user settings, and triggering notification sends added to FE pushTokenService: 'getPushTokensAnsSendNotification' - stop adding 1000 to ID for development. We're handling this in startRedis now

--- frontend/src/app/shared/utils/pushTokenService.ts
+++ frontend/src/app/shared/utils/pushTokenService.ts
@@ -1,6 +1,12 @@
 import { Injectable, WritableSignal, effect, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Store } from '@ngrx/store';
+import {
+  selectFriendByUserID,
+  selectFriends,
+} from 'src/app/profile/state/profile-selectors';
+import { Observable, catchError, filter, from, mergeMap, of, take } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +15,7 @@ export class PushTokenService {
   private API_URL = `${environment.BACKEND}/pushTokens`;
   public unsavedPushToken: WritableSignal<string | null> = signal(null);
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private store: Store) {}
 
   public savePushToken(token: string) {
     this.http.post(this.API_URL, { token }).subscribe(
@@ -36,5 +42,48 @@ export class PushTokenService {
       type,
       data,
     });
+  }
+
+  public getPushTokensAndSendNotification(
+    settingName,
+    type,
+    data
+  ): Observable<any> {
+    return this.store.select(selectFriends).pipe(
+      take(1),
+      catchError((error) => {
+        console.error('Error selecting friends: ', error);
+        return of([]);
+      }),
+      mergeMap((friends) => from(friends)),
+      mergeMap((friend: any) =>
+        this.store.select(selectFriendByUserID(friend.userID)).pipe(
+          take(1),
+          filter(
+            (friendProfile) =>
+              !(
+                friendProfile[settingName] === 'None' ||
+                friendProfile[settingName] === 'Email Only'
+              )
+          ),
+          mergeMap((friendProfile) =>
+            this.getOtherUserPushTokens(friendProfile.userID).pipe(
+              catchError((error) => {
+                console.error('Error getting push tokens for user: ', error);
+                return of([]);
+              })
+            )
+          ),
+          mergeMap((tokens) =>
+            this.sendPushNotification(tokens, type, data).pipe(
+              catchError((error) => {
+                console.error('Error sending push notification: ', error);
+                return of(null);
+              })
+            )
+          )
+        )
+      )
+    );
   }
 }
