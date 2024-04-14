c367824922af050e9300902011c1b2976741e97b
Josh Merrell <27826752+josh-merrell@users.noreply.github.com>
2024-04-14 14:06:43 -0700

    Josh (#318)
    * 04022024:
    - autoDeleteListRecipe, provide reason in Confirmation Modal
    - fix Followers Page styling, Search now always visible
    
    * - missed staging files in previous commit
    - restyled all ingredient/tool/ingredientStock/toolStock/recipeIng/recipeTool/recipeStep/recipeEdit/addListIngr/useRecipe Modals
    
    * - adding app messages for 'ingredientStockExpired', 'ingredientOutOfStock', 'newFollower', 'newFriend', and 'newFriendRequest' events in backent. Tested all

--- server/middleware/validating.js
+++ server/middleware/validating.js
@@ -6,7 +6,7 @@ function routeValidator(schema, dataToValidate) {
     const data = req[dataToValidate];
     const valid = ajv.validate(schema, data);
     if (!valid) {
-      global.logger.info(`INVALID DATA '${JSON.stringify(data)}' IN REQUEST ${data}. ${ajv.errorsText()}.`);
+      global.logger.error(`INVALID DATA '${JSON.stringify(data)}' IN REQUEST ${data}. ${ajv.errorsText()}.`);
       return res.status(422).json({ error: `${ajv.errors[0].dataPath.slice(1)} ${ajv.errors[0].message}` });
     }
     if (!req.userID) {
