de502d5c01c854998ccaa4703d7182a7457da776
Josh Merrell <joshmerrell.us@gmail.com>
2024-04-21 16:57:18 -0700

    - test

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -187,7 +187,7 @@ module.exports = {
         "dl-15": "36rem",
         "safe-top": "env(safe-area-inset-top)",
         "safe-right": "env(safe-area-inset-right)",
-        "safe-bottom": "calc(env(safe-area-inset-bottom) + 0.5rem)",
+        "safe-bottom": "calc(env(safe-area-inset-bottom) + 1rem)",
       },
       padding: {
         "dl-0": "0rem",
