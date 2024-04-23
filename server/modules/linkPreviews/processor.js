f832c986b13a1a2b8bf6e6654ef27480bb094c82
Josh Merrell <27826752+josh-merrell@users.noreply.github.com>
2024-04-23 15:47:46 -0700

    Josh (#340)
    * - testing returning recipe html for link previews
    
    * - test adding image width/height tags for preview

--- server/modules/linkPreviews/processor.js
+++ server/modules/linkPreviews/processor.js
@@ -6,7 +6,7 @@ module.exports = ({ db, dbPublic }) => {
     try {
       const recipeData = {
         title: 'Veg Club Sandwich',
-        description: 'Check out my new recipe on Doughly.',
+        description: 'Check out my new recipe on Doughly. This is a delicious veg club sandwich that is perfect for a quick lunch or dinner.',
         image: 'https://s3.us-west-2.amazonaws.com/dl.images/recipe/ade96f70-4ec5-4ab9-adfe-0645b16e1ced/1000021838.jpg',
         url: `https://doughly.co/recipe/public/1124033000000001`,
       };
@@ -20,11 +20,15 @@ module.exports = ({ db, dbPublic }) => {
               <meta property="og:title" content="${recipeData.title}" />
               <meta property="og:description" content="${recipeData.description}" />
               <meta property="og:image" content="${recipeData.image}" />
+              <meta property="og:image:width" content="375" />  
+              <meta property="og:image:height" content="225" /> 
               <meta property="og:url" content="${recipeData.url}" />
               <meta property="og:type" content="website" />
               <meta charset="utf-8">
             </head>
-            <html>`;
+            <body>
+            </body>
+            </html>`;
 
       return htmlContent;
     } catch (e) {
