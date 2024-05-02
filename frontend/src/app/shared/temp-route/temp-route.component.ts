c5c21e962e139ec8aa7975609b50daec7a834e6e
Josh Merrell <27826752+josh-merrell@users.noreply.github.com>
2024-05-01 22:45:00 -0700

    Josh (#387)
    * - fixed message generation
    
    * 05012024:
    - onboarding done to step 12 of 15 (needs more complete testing)

--- frontend/src/app/shared/temp-route/temp-route.component.ts
+++ frontend/src/app/shared/temp-route/temp-route.component.ts
@@ -1,27 +1,167 @@
-import { Component } from '@angular/core';
+import { Component, WritableSignal, signal } from '@angular/core';
 import { RedirectPathService } from 'src/app/shared/utils/redirect-path.service';
 import { Router, RouterLinkWithHref } from '@angular/router';
 import { AuthService } from '../utils/authenticationService';
+import { ExtraStuffService } from '../utils/extraStuffService';
+import { Store } from '@ngrx/store';
+import { ProfileActions } from 'src/app/profile/state/profile-actions';
+import {
+  selectError,
+  selectProfile,
+  selectUpdating,
+} from 'src/app/profile/state/profile-selectors';
+import { filter, take } from 'rxjs';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'dl-temp-route',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatProgressSpinnerModule],
   templateUrl: './temp-route.component.html',
 })
 export class TempRouteComponent {
+  isLoading: WritableSignal<boolean> = signal(true);
+  private profile: WritableSignal<any> = signal(null);
   constructor(
     private redirectPathService: RedirectPathService,
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    private extraStuffService: ExtraStuffService,
+    private store: Store
   ) {}
 
   ngOnInit(): void {
+    this.store.select(selectProfile).subscribe((profile) => {
+      this.profile.set(profile);
+    });
     const path = this.redirectPathService.getPath();
-    const profile = this.authService.profile();
+    const profile = this.profile();
     switch (profile?.onboardingState) {
-      case 1:
+      case 1 || 2:
         this.router.navigate(['/recipes/discover']);
+        break;
+      case 3 || 4:
+        if (this.extraStuffService.onboardingPublicRecipe()) {
+          this.router.navigate([
+            '/recipe/public/' + this.extraStuffService.onboardingPublicRecipe(),
+          ]);
+        } else {
+          // set onboardingState back to 2 to have user select a recipe for routing again
+          this.isLoading.set(true);
+          console.log('onboardingPublicRecipe not set');
+          this.store.dispatch(
+            ProfileActions.updateProfileProperty({
+              property: 'onboardingState',
+              value: 2,
+            })
+          );
+          this.store
+            .select(selectUpdating)
+            .pipe(
+              filter((updating) => !updating),
+              take(1)
+            )
+            .subscribe(() => {
+              this.store
+                .select(selectError)
+                .pipe(take(1))
+                .subscribe((error) => {
+                  if (error) {
+                    console.error(
+                      `Error updating onboarding state: ${error.message}, CODE: ${error.statusCode}`
+                    );
+                  } else {
+                    this.isLoading.set(false);
+                    this.router.navigate(['/recipes/discover']);
+                  }
+                });
+            });
+        }
+        break;
+      case 5:
+        if (path === '/social') {
+          // advance onboardingState to 6
+          this.isLoading.set(true);
+          this.store.dispatch(
+            ProfileActions.updateProfileProperty({
+              property: 'onboardingState',
+              value: 6,
+            })
+          );
+          this.store
+            .select(selectUpdating)
+            .pipe(
+              filter((updating) => !updating),
+              take(1)
+            )
+            .subscribe(() => {
+              this.store
+                .select(selectError)
+                .pipe(take(1))
+                .subscribe((error) => {
+                  if (error) {
+                    console.error(
+                      `Error updating onboarding state: ${error.message}, CODE: ${error.statusCode}`
+                    );
+                  } else {
+                    this.isLoading.set(false);
+                    this.router.navigate(['/social']);
+                  }
+                });
+            });
+        } else if (this.extraStuffService.onboardingSubscribedRecipe()) {
+          console.log('onboardingSubscribedRecipe set, routing to it');
+          this.router.navigate([
+            '/recipe/' + this.extraStuffService.onboardingSubscribedRecipe(),
+          ]);
+        } else {
+          // set onboardingState back to 3 to have user select a recipe for routing again
+          this.isLoading.set(true);
+          console.log('onboardingSubscribedRecipe not set');
+          this.store.dispatch(
+            ProfileActions.updateProfileProperty({
+              property: 'onboardingState',
+              value: 3,
+            })
+          );
+          this.store
+            .select(selectUpdating)
+            .pipe(
+              filter((updating) => !updating),
+              take(1)
+            )
+            .subscribe(() => {
+              this.store
+                .select(selectError)
+                .pipe(take(1))
+                .subscribe((error) => {
+                  if (error) {
+                    console.error(
+                      `Error updating onboarding state: ${error.message}, CODE: ${error.statusCode}`
+                    );
+                  } else {
+                    this.isLoading.set(false);
+                    this.router.navigate(['/tempRoute'], { onSameUrlNavigation: 'reload' });
+                  }
+                });
+            });
+        }
+        break;
+      case 6:
+        this.router.navigate(['/social']);
+        break;
+      case 7:
+        this.router.navigate(['/groceries']);
+        break;
+      case 8:
+        this.router.navigate(['/recipes/created']);
+        break;
+      case 9 | 10 | 11:
+        this.router.navigate(['/recipes/created/add']);
+        break;
+      case 12:
+        this.router.navigate(['/recipes/created/add/image']);
         break;
       default: // onboardingState 0 (done)
         if (path) {
