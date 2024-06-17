a134e604062926a59c99462ec6e3497e731b7a72
Josh Merrell <joshmerrell.us@gmail.com>
2024-06-16 21:48:03 -0700

    in progress

--- server/modules/shopping/listIngredients/processor.js
+++ server/modules/shopping/listIngredients/processor.js
@@ -17,10 +17,9 @@ module.exports = ({ db }) => {
         .filter('deleted', 'eq', false)
         .single();
       if (error) {
-        global.logger.error(`Error getting shopping list ingredient ${shoppingListIngredientID}: ${error.message}`);
-        throw errorGen(`Error getting shopping list ingredient ${shoppingListIngredientID}`, 400);
+        throw errorGen(`Error getting shopping list ingredient ${shoppingListIngredientID}: ${error.message}`, 511, 'failSupabaseSelect', true, 3);
       }
-      global.logger.info('Got shoppingListIngredient');
+      global.logger.info({ message: `Got shoppingListIngredient`, level: 6, timestamp: new Date().toISOString(), userID: shoppingListIngredient.userID });
       return shoppingListIngredient;
     } catch (err) {
       throw errorGen(err.message || 'Unhandled Error in listIngredients getShoppingListIngredientByID', err.code || 520, err.name || 'unhandledError_listIngredients-getShoppingListIngredientByID', err.isOperational || false, err.severity || 2);
@@ -37,10 +36,9 @@ module.exports = ({ db }) => {
         .filter('shoppingListID', 'eq', shoppingListID)
         .filter('deleted', 'eq', false);
       if (error) {
-        global.logger.error(`Error getting shopping list ingredients: ${error.message}`);
-        throw errorGen(`Error getting shopping list ingredients`, 400);
+        throw errorGen(`Error getting shopping list ingredients: ${error.message}`, 511, 'failSupabaseSelect', true, 3);
       }
-      global.logger.info(`Got ${shoppingListIngredients.length} ingredients for shoppingList ${shoppingListID}`);
+      global.logger.info({ message: ``, level: 6, timestamp: new Date().toISOString(), userID: shoppingListIngredients[0].userID || 0 });
       return shoppingListIngredients;
     } catch (err) {
       throw errorGen(err.message || 'Unhandled Error in listIngredients getIngredientsByShoppingList', err.code || 520, err.name || 'unhandledError_listIngredients-getIngredientsByShoppingList', err.isOperational || false, err.severity || 2);
@@ -52,48 +50,40 @@ module.exports = ({ db }) => {
 
     try {
       if (!customID) {
-        global.logger.error('customID is missing');
-        throw errorGen('customID is missing', 400);
+        throw errorGen(`customID is missing, can't create shoppingListIngredient`, 510, 'dataValidationErr', false, 3);
       }
 
       //verify that provided shoppingList exists and is in draft status
       const { data: shoppingList, error: shoppingListError } = await db.from('shoppingLists').select('shoppingListID').filter('shoppingListID', 'eq', shoppingListID).filter('status', 'eq', 'draft');
       if (shoppingListError) {
-        global.logger.error(`Error getting shoppingList: ${shoppingListError.message}`);
-        throw errorGen(`Error getting shoppingList: ${shoppingListError.message}`, 400);
+        throw errorGen(`Error getting shoppingList: ${shoppingListError.message}`, 511, 'failSupabaseSelect', true, 3);
       }
       if (shoppingList.length === 0) {
-        global.logger.error('shoppingList does not exist or is not in draft status');
-        throw errorGen('shoppingList does not exist or is not in draft status', 400);
+        throw errorGen(`shoppingList does not exist or is not in draft status, can't create shoppingListIngredient`, 515, 'cannotComplete', false, 3);
       }
 
       //verify that provided ingredient exists
       const { data: ingredient, error: ingredientError } = await db.from('ingredients').select('ingredientID').filter('ingredientID', 'eq', ingredientID).filter('deleted', 'eq', false);
       if (ingredientError) {
-        global.logger.error(`Error getting ingredient: ${ingredientError.message}`);
-        throw errorGen(`Error getting ingredient: ${ingredientError.message}`, 400);
+        throw errorGen(`Error getting ingredient: ${ingredientError.message}`, 511, 'failSupabaseSelect', true, 3);
       }
       if (ingredient.length === 0) {
-        global.logger.error(`ingredient does not exist`);
-        throw errorGen(`ingredient does not exist`, 400);
+        throw errorGen(`ingredient does not exist, can't create shoppingListIngredient`, 515, 'cannotComplete', false, 3);
       }
 
       //verify no other shoppingListIngredient exists for this shoppingList and ingredient
       const { data: existingShoppingListIngredient, error: existingShoppingListIngredientError } = await db.from('shoppingListIngredients').select('shoppingListIngredientID').filter('shoppingListID', 'eq', shoppingListID).filter('ingredientID', 'eq', ingredientID).filter('deleted', 'eq', false);
       if (existingShoppingListIngredientError) {
-        global.logger.error(`error getting existing shoppingListIngredients`);
-        throw errorGen(`error getting existing shoppingListIngredients`, 400);
+        throw errorGen(`error getting existing shoppingListIngredients when creating shoppingListIngredient`, 511, 'failSupabaseSelect', true, 3);
       }
       if (existingShoppingListIngredient.length > 0) {
-        global.logger.error(`this ingredient already exists on this shoppingList`);
-        throw errorGen(`this ingredient already exists on this shoppingList`, 400);
+        throw errorGen(`this ingredient already exists on this shoppingList`, 515, 'cannotComplete', false, 3);
       }
 
       // create shoppingListIngredient
       const { data: shoppingListIngredient, error: shoppingListIngredientError } = await db.from('shoppingListIngredients').insert({ userID, shoppingListIngredientID: customID, shoppingListID, ingredientID, needMeasurement, needUnit, source }).select('*').single();
       if (shoppingListIngredientError) {
-        global.logger.error(`Error creating shoppingListIngredient with ID ${customID}: ${shoppingListIngredientError.message}`);
-        throw errorGen(`Error creating shoppingListIngredient with ID ${customID}: ${shoppingListIngredientError.message}`, 400);
+        throw errorGen(`Error creating shoppingListIngredient with ID ${customID}: ${shoppingListIngredientError.message}`, 512, 'failSupabaseInsert', true, 3);
       }
       //add a 'addedIngredientToShoppingList' log
       await createShoppingLog(userID, authorization, 'addIngredientToShoppingList', Number(shoppingListIngredient.shoppingListIngredientID), Number(shoppingListID), null, null, `addedIngredientToShoppingList: ${shoppingListIngredient.shoppingListIngredientID}`);
@@ -111,23 +101,19 @@ module.exports = ({ db }) => {
       //verify that provided shoppingListIngredient exists
       const { data: shoppingListIngredient, error: shoppingListIngredientError } = await db.from('shoppingListIngredients').select('*').filter('shoppingListIngredientID', 'eq', shoppingListIngredientID).filter('deleted', 'eq', false);
       if (shoppingListIngredientError) {
-        global.logger.error(`Error getting shoppingListIngredient: ${shoppingListIngredientError.message}`);
-        throw errorGen(`Error getting shoppingListIngredient: ${shoppingListIngredientError.message}`, 400);
+        throw errorGen(`Error getting shoppingListIngredient: ${shoppingListIngredientError.message}`, 511, 'failSupabaseSelect', true, 3);
       }
       if (shoppingListIngredient.length === 0) {
-        global.logger.error('shoppingListIngredient does not exist');
-        throw errorGen('shoppingListIngredient does not exist', 400);
+        throw errorGen(`shoppingListIngredient does not exist, can't update`, 515, 'cannotComplete', false, 3);
       }
 
       //verify that provided shoppingList exists and is not deleted
       const { data: shoppingList, error: shoppingListError } = await db.from('shoppingLists').select('shoppingListID').filter('shoppingListID', 'eq', shoppingListIngredient[0].shoppingListID).filter('status', 'neq', 'deleted');
       if (shoppingListError) {
-        global.logger.error(`Error getting shoppingList: ${shoppingListError.message}`);
-        throw errorGen(`Error getting shoppingList: ${shoppingListError.message}`, 400);
+        throw errorGen(`Error getting shoppingList: ${shoppingListError.message}`, 511, 'failSupabaseSelect', true, 3);
       }
       if (shoppingList.length === 0) {
-        global.logger.error('shoppingList does not exist');
-        throw errorGen('shoppingList does not exist', 400);
+        throw errorGen(`shoppingList does not exist`, 515, 'cannotComplete', false, 3);
       }
 
       //update shoppingListIngredient
@@ -151,30 +137,25 @@ module.exports = ({ db }) => {
       //verify that provided shoppingListIngredient exists
       const { data: shoppingListIngredient, error: shoppingListIngredientError } = await db.from('shoppingListIngredients').select('*').filter('shoppingListIngredientID', 'eq', shoppingListIngredientID).filter('deleted', 'eq', false);
       if (shoppingListIngredientError) {
-        global.logger.error(`Error getting shoppingListIngredient: ${shoppingListIngredientError.message}`);
-        throw errorGen(`Error getting shoppingListIngredient: ${shoppingListIngredientError.message}`, 400);
+        throw errorGen(`Error getting shoppingListIngredient: ${shoppingListIngredientError.message}`, 511, 'failSupabaseSelect', true, 3);
       }
       if (shoppingListIngredient.length === 0) {
-        global.logger.error('shoppingListIngredient does not exist');
-        return { error: 'shoppingListIngredient does not exist' };
+        throw errorGen(`shoppingListIngredient does not exist, can't delete`, 515, 'cannotComplete', false, 3);
       }
 
       //verify that provided shoppingList exists
       const { data: shoppingList, error: shoppingListError } = await db.from('shoppingLists').select('shoppingListID, status').filter('shoppingListID', 'eq', shoppingListIngredient[0].shoppingListID);
       if (shoppingListError) {
-        global.logger.error(`Error getting shoppingList: ${shoppingListError.message}`);
-        throw errorGen(`Error getting shoppingList: ${shoppingListError.message}`, 400);
+        throw errorGen(`Error getting shoppingList: ${shoppingListError.message}`, 511, 'failSupabaseSelect', true, 3);
       }
       if (shoppingList.length === 0) {
-        global.logger.error('shoppingList does not exist');
-        return { error: 'shoppingList does not exist' };
+        throw errorGen(`shoppingList does not exist, cannot delete shoppingListIngredient`, 515, 'cannotComplete', false, 3);
       }
 
       //delete shoppingListIngredient
       const { error: deleteError } = await db.from('shoppingListIngredients').update({ deleted: true }).filter('shoppingListIngredientID', 'eq', shoppingListIngredientID);
       if (deleteError) {
-        global.logger.error(`Error deleting shoppingListIngredient: ${deleteError.message}`);
-        throw errorGen(`Error deleting shoppingListIngredient: ${deleteError.message}`, 400);
+        throw errorGen(`Error deleting shoppingListIngredient: ${deleteError.message}`, 513, 'failSupabaseUpdate', true, 3);
       }
 
       //if shoppingList is in 'shopping' status, get any remaining shoppingListIngredients
@@ -186,16 +167,14 @@ module.exports = ({ db }) => {
           .filter('deleted', 'eq', false)
           .is('store', null);
         if (remainingShoppingListIngredientsError) {
-          global.logger.error(`Error getting remaining shoppingListIngredients: ${remainingShoppingListIngredientsError}`);
-          throw errorGen(`Error getting remaining shoppingListIngredients: ${remainingShoppingListIngredientsError}`, 400);
+          throw errorGen(`Error getting remaining shoppingListIngredients: ${remainingShoppingListIngredientsError}`, 511, 'failSupabaseSelect', true, 3);
         }
         //if none remaining, delete any shoppingListRecipes and standalone ingredients, then set shoppingList status to 'draft'
         if (remainingShoppingListIngredients.length === 0) {
           //delete any shoppingListRecipes for this shoppingList using axios calls
           const { data: shoppingListRecipes, error: shoppingListRecipesError } = await db.from('shoppingListRecipes').select('shoppingListRecipeID').filter('shoppingListID', 'eq', shoppingListIngredient[0].shoppingListID).filter('deleted', 'eq', false);
           if (shoppingListRecipesError) {
-            global.logger.error(`Error getting shoppingListRecipes while clearing shoppingList ID: ${shoppingListIngredient[0].shoppingListID}: ${shoppingListRecipesError.message}`);
-            throw errorGen(`Error getting shoppingListRecipes while clearing shoppingList ID: ${shoppingListIngredient[0].shoppingListID}: ${shoppingListRecipesError.message}`, 400);
+            throw errorGen(`Error getting shoppingListRecipes while clearing shoppingList ID: ${shoppingListIngredient[0].shoppingListID}: ${shoppingListRecipesError.message}`, 511, 'failSupabaseSelect', true, 3);
           }
           if (shoppingListRecipes.length > 0) {
             for (let i = 0; i < shoppingListRecipes.length; i++) {
@@ -210,8 +189,13 @@ module.exports = ({ db }) => {
                 },
               });
               if (deleteShoppingListRecipeError) {
-                global.logger.error(`Error deleting shoppingListRecipe ID: ${shoppingListRecipes[i].shoppingListRecipeID}: ${deleteShoppingListRecipeError.message}`);
-                throw errorGen(`Error deleting shoppingListRecipe ID: ${shoppingListRecipes[i].shoppingListRecipeID}: ${deleteShoppingListRecipeError.message}`, 400);
+                throw errorGen(
+                  deleteShoppingListRecipeError.message || `Error deleting shoppingListRecipe ID: ${shoppingListRecipes[i].shoppingListRecipeID}: ${deleteShoppingListRecipeError.message}`,
+                  deleteShoppingListRecipeError.code || 520,
+                  deleteShoppingListRecipeError.name || 'unhandledError_listIngredients-updateShoppingListIngredient',
+                  deleteShoppingListRecipeError.isOperational || false,
+                  deleteShoppingListRecipeError.severity || 2,
+                );
               }
             }
           }
@@ -219,8 +203,7 @@ module.exports = ({ db }) => {
           //delete any standalone shoppingListIngredients for this shoppingList using axios calls
           const { data: shoppingListIngredients, error: shoppingListIngredientsError } = await db.from('shoppingListIngredients').select('shoppingListIngredientID').filter('shoppingListID', 'eq', shoppingListIngredient[0].shoppingListID).filter('deleted', 'eq', false);
           if (shoppingListIngredientsError) {
-            global.logger.error(`Error getting standalone ingredients while clearing shoppingList ID: ${shoppingListIngredient[0].shoppingListID}: ${shoppingListIngredientsError.message}`);
-            throw errorGen(`Error getting standalone ingredients while clearing shoppingList ID: ${shoppingListIngredient[0].shoppingListID}: ${shoppingListIngredientsError.message}`, 400);
+            throw errorGen(`Error getting standalone ingredients while clearing shoppingList ID: ${shoppingListIngredient[0].shoppingListID}: ${shoppingListIngredientsError.message}`, 511, 'failSupabaseSelect', true, 3);
           }
           if (shoppingListIngredients.length > 0) {
             for (let i = 0; i < shoppingListIngredients.length; i++) {
@@ -235,8 +218,13 @@ module.exports = ({ db }) => {
                 },
               });
               if (deleteShoppingListIngredientError) {
-                global.logger.errror(`Error deleting shoppingListIngredient ID: ${shoppingListIngredients[i].shoppingListIngredientID}: ${deleteShoppingListIngredientError.message}`);
-                throw errorGen(`Error deleting shoppingListIngredient ID: ${shoppingListIngredients[i].shoppingListIngredientID}: ${deleteShoppingListIngredientError.message}`, 400);
+                throw errorGen(
+                  deleteShoppingListIngredientError.message || `Error deleting shoppingListIngredient ID: ${shoppingListRecipes[i].shoppingListRecipeID}: ${deleteShoppingListRecipeError.message}`,
+                  deleteShoppingListIngredientError.code || 520,
+                  deleteShoppingListIngredientError.name || 'unhandledError_listIngredients-deleteShoppingListIngredient',
+                  deleteShoppingListIngredientError.isOperational || false,
+                  deleteShoppingListIngredientError.severity || 2,
+                );
               }
             }
           }
