5ce2b74b0f9daf65859b3acdab6f16b5deb2d272
Josh Merrell <joshmerrell.us@gmail.com>
2024-06-23 17:03:42 -0700

    - fixed bug for editRecipeIngredient measurement form value

--- frontend/src/app/groceries/feature/shared-shopping-lists-page/shared-shopping-lists-page.component.ts
+++ frontend/src/app/groceries/feature/shared-shopping-lists-page/shared-shopping-lists-page.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, WritableSignal, signal } from '@angular/core';
 
 @Component({
   selector: 'dl-shared-shopping-lists-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './shared-shopping-lists-page.component.html',
 })
 export class SharedShoppingListsPageComponent {
-
+  public isLoading: WritableSignal<boolean> = signal(false);
+  public displaySharedShoppingLists: WritableSignal<any[]> = signal([]);
 }
