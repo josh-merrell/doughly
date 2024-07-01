5bc48a07696053a68ac86b9a11ef317aaad5e249
Josh Merrell <joshmerrell.us@gmail.com>
2024-07-01 16:34:48 -0700

    - testing deploy script adjustments

--- server/userdata-api.sh
+++ server/userdata-api.sh
@@ -1,3 +1,5 @@
+# Run this script as the user-data script for the API server instance upon initial launch. This script will install the necessary tools, clone the application repository, install dependencies, and start the application using PM2. When code changes are merged, a separate deployment script will be run to update the application on all current EC2 instances in the deployment group. This is managed with AWS CodeDeploy via the 'appspec.yml' file.
+
 #!/bin/bash
 set -e
 
