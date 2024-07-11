89049b5b9ca64052cafb163fe501c8909007b92a
Josh Merrell <27826752+josh-merrell@users.noreply.github.com>
2024-07-11 11:58:04 -0700

    - ~ (#535)

--- server/scripts/start_application.sh
+++ server/scripts/start_application.sh
@@ -8,23 +8,13 @@ sudo chown -R ubuntu:ubuntu /home/ubuntu/.npm
 cd /home/ubuntu/dl
 sudo chmod +x /home/ubuntu/dl/scripts/*
 
-# Remove old node_modules with sudo and ensure correct permissions
-sudo rm -rf node_modules package-lock.json
-sudo chown -R ubuntu:ubuntu /home/ubuntu/dl
-
-# Install dependencies
-npm install
-npx playwright install-deps
-npx playwright install
-
 # Ensure the ubuntu user has ownership of the .pm2 directory
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.pm2
+# Ensure the ubuntu user has ownership of the application directory
+sudo chown -R ubuntu:ubuntu /home/ubuntu/dl
 
 # Navigate to the application directory
 cd /home/ubuntu/dl
-
-# Ensure the ubuntu user has ownership of the application directory
-sudo chown -R ubuntu:ubuntu /home/ubuntu/dl
 
 # Restart the application using PM2
 npx pm2 stop ecosystem.config.js || true
