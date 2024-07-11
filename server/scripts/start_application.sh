11fa19bbad36a305692e1dfa2725f0f955cd049e
Josh Merrell <joshmerrell.us@gmail.com>
2024-07-10 23:05:50 -0700

    - ~

--- server/scripts/start_application.sh
+++ server/scripts/start_application.sh
@@ -6,6 +6,7 @@ sudo chown -R ubuntu:ubuntu /home/ubuntu/.npm
 
 # Navigate to the application directory
 cd /home/ubuntu/dl
+sudo chmod +x /home/ubuntu/dl/scripts/*
 
 # Remove old node_modules with sudo and ensure correct permissions
 sudo rm -rf node_modules package-lock.json
