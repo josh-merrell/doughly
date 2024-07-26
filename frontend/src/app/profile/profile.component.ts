3801b07e82a54766c5fd884db17cdb7aa2afc77e
Josh Merrell <joshmerrell.us@gmail.com>
2024-07-25 17:09:11 -0700

    - use toggles for notification settings rather than dropdowns

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -51,7 +51,7 @@ export class ProfileComponent {
     effect(() => {
       const profile = this.authService.profile();
       if (profile) {
-        console.log('profile', profile);
+        console.log('PROFILE', profile);
         this.profile = profile;
         this.profileImageLink = profile?.photo_url;
         this.initials =
@@ -129,19 +129,25 @@ export class ProfileComponent {
 
     if (ref) {
       ref.afterClosed().subscribe((result) => {
-        if (result === 'cancel' || !result)
-          if (result === 'success') {
-            this.modalService.open(
-              ConfirmationModalComponent,
-              {
-                data: {
-                  confirmationMessage: 'Profile photo updated successfully!',
-                },
+        if (result === 'cancel' || !result) {
+          return;
+        }
+        if (result === 'success') {
+          this.modalService.open(
+            ConfirmationModalComponent,
+            {
+              data: {
+                confirmationMessage: 'Profile photo updated successfully!',
               },
-              1,
-              true
-            );
-          }
+            },
+            1,
+            true
+          );
+          // wait 10 seconds, then refresh profile
+          setTimeout(() => {
+            this.authService.refreshProfile();
+          }, 10000);
+        }
       });
     } else {
     }
